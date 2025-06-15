ab196c17757e9abdfea1ece4f8da49bc294bee43
RamaAdiSatria <149090409+ramrambgr@users.noreply.github.com>
2025-06-15 16:14:24 +0700

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ import streamlit as st
 import google.generativeai as genai
 
 def load_gemini_model(api_key=None):
-    api_key = api_key or st.secrets["AIzaSyCqcMN3u6xQNAvPc-kdff5N_GKFsCadOMo"]
+    api_key = api_key or st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
     return genai.GenerativeModel("gemini-1.5-flash-latest")
