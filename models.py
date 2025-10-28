04cdb5f1a6a3b57d568d1c4cbb584cae26809705
RamaAdiSatria <149090409+ramrambgr@users.noreply.github.com>
2025-10-28 19:40:42 +0700

    Update models.py

--- models.py
+++ models.py
@@ -4,4 +4,4 @@ import google.generativeai as genai
 def load_gemini_model(api_key=None):
     api_key = api_key or st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    return genai.GenerativeModel("gemini-1.5-flash-latest")
+    return genai.GenerativeModel("gemini-1.5-flash")
